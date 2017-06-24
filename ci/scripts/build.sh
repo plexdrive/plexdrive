d30b9a0a11d0d4572919e10d4a77f3ddbbc9f636
Dominik Weidenfeld <dom.weidenfeld@gmail.com>
2017-06-24 21:54:49 +0200

    added cross compile automation

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -xe
 
+export BUILDSCRIPT=$PWD/go-build-all.sh
 export GOPATH=$PWD/go
 export PATH=$GOPATH/bin:$PATH
 
 cd $GOPATH/src/github.com/dweidenfeld/plexdrive
 
 go get
-go build -o $GOPATH/binary/plexdrive
+./ci/scripts/go-build-all
+
+mkdir $GOPATH/release
+mv plexdrive-* $GOPATH/release
