1495dd1cbdb31676bf369bf0214ca53703c8c90a
Dominik Weidenfeld <dom.weidenfeld@gmail.com>
2017-06-24 22:28:06 +0200

    preparation of deploy target

---
+++ ci/scripts/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -xe
+
+ls -lah
+
+# export GOPATH=$PWD/go
+# export PATH=$GOPATH/bin:$PATH
+
+# cd $GOPATH/src/github.com/dweidenfeld/plexdrive
+
+# go get -v
+# go test ./... -race -cover
