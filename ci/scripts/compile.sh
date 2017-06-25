66916fc545e9530ce1c465b92a374b5fce7ea1be
Dominik Weidenfeld <dom.weidenfeld@gmail.com>
2017-06-25 15:33:53 +0200

    fixed notification area

--- ci/scripts/compile.sh
+++ ci/scripts/compile.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 set -xe
 
+# Configuration
 export ORIGIN=$PWD
 export GOPATH=$PWD/go
 export PATH=$GOPATH/bin:$PATH
 export TS=$(date +%s)
-
 cd $GOPATH/src/github.com/dweidenfeld/plexdrive
 
+# Version
 export VERSION="$(cat docs/version)-$TS"
 echo "Got version $VERSION"
 
 sed -i.bak s/%VERSION%/$VERSION/g main.go
+echo $VERSION > $ORIGIN/metadata/version
+sed s/%VERSION%/$VERSION/g docs/notification > $ORIGIN/metadata/notification
 
+# Build 
 go get -v
 ./ci/scripts/go-build-all
 
 mv plexdrive-* $ORIGIN/release
-echo $VERSION > $ORIGIN/metadata/version
 
+# Check
 cd $ORIGIN
 ls -lah release
 ls -lah metadata
