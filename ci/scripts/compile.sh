81451d97b1d0345f2809e51bb6a44db7661c6816
Dominik Weidenfeld <dom.weidenfeld@gmail.com>
2017-06-25 14:33:48 +0200

    version timestamp + slack notification

--- ci/scripts/compile.sh
+++ ci/scripts/compile.sh
@@ -4,13 +4,16 @@ set -xe
 export ORIGIN=$PWD
 export GOPATH=$PWD/go
 export PATH=$GOPATH/bin:$PATH
+export TS=$(date +%s)
 
 cd $GOPATH/src/github.com/dweidenfeld/plexdrive
 
-export VERSION=$(cat docs/version)
+export VERSION="$(cat docs/version)-$TS"
 echo "Got version $VERSION from docs/version"
+echo $VERSION > docs/version
 
 sed -i.bak s/%VERSION%/$VERSION/g main.go
+sed -i.bak s/%VERSION%/$VERSION/g docs/slack-notification
 
 go get -v
 ./ci/scripts/go-build-all
