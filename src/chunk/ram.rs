1724b30dd9cb916618447fb34ec770693e4d06f3
Dominik Weidenfeld <dominik.weidenfeld@twt.de>
2017-08-11 17:49:16 +0200

    multi threaded non blocking

--- src/chunk/ram.rs
+++ src/chunk/ram.rs
@@ -1,14 +1,19 @@
+use std::sync::{Arc, Mutex};
+use std::collections::HashMap;
+
 use api;
 use chunk;
 
 pub struct RAMManager<C> {
-    client: C
+    client: C,
+    chunks: Mutex<HashMap<String, Vec<u8>>>
 }
 
 impl<C> RAMManager<C> where C: api::Client {
     pub fn new(client: C) -> chunk::ChunkResult<RAMManager<C>> {
         Ok(RAMManager {
-            client
+            client: client,
+            chunks: Mutex::new(HashMap::new()),
         })
     }
 }
@@ -17,6 +22,9 @@ impl<C> chunk::Manager for RAMManager<C> where C: api::Client {
     fn get_chunk<F>(&self, url: &str, start: u64, offset: u64, callback: F)
         where F: FnOnce(chunk::ChunkResult<Vec<u8>>) + Send + 'static
     {
+        let mut chunks = self.chunks.lock().unwrap();
+        // TODO: resume here
+
         match self.client.do_http_request(url, start, start + offset) {
             Ok(chunk) => callback(Ok(chunk)),
             Err(cause) => {
--- src/chunk/thread.rs
+++ src/chunk/thread.rs
@@ -1,26 +1,26 @@
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 use threadpool;
 
 use chunk;
 
 pub struct ThreadManager<M> {
-    manager: Arc<Mutex<Box<M>>>,
+    manager: Arc<M>,
     pool: threadpool::ThreadPool,
 }
 
 impl<M> ThreadManager<M>
-    where M: chunk::Manager + Send + 'static
+    where M: chunk::Manager + Sync + Send + 'static
 {
     pub fn new(threads: usize, manager: M) -> chunk::ChunkResult<ThreadManager<M>> {
         Ok(ThreadManager {
-               manager: Arc::new(Mutex::new(Box::new(manager))),
+               manager: Arc::new(manager),
                pool: threadpool::ThreadPool::new(threads),
            })
     }
 }
 
 impl<M> chunk::Manager for ThreadManager<M>
-    where M: chunk::Manager + Send + 'static
+    where M: chunk::Manager + Sync + Send + 'static
 {
     fn get_chunk<F>(&self, url: &str, start: u64, offset: u64, callback: F)
         where F: FnOnce(chunk::ChunkResult<Vec<u8>>) + Send + 'static
@@ -31,7 +31,7 @@ impl<M> chunk::Manager for ThreadManager<M>
         let offset = offset.clone();
 
         self.pool.execute(move || {
-                              manager.lock().unwrap().get_chunk(&url, start, offset, callback);
-                          });
+            manager.get_chunk(&url, start, offset, callback);
+        });
     }
 }
